1df630e7ac24829738f531872d3448e379a6dedf
Sola <solalala@systemli.org>
2024-10-28 20:26:56 +0100

    WIP

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -41,8 +41,8 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-                         'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'email_verified_at' => 'datetime',
+                         'password' => 'hashed',
         ];
     }
 }
