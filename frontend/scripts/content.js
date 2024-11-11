a095d8a659e7321fa6136a460fa04b297b0ca708
Fili <fili@systemli.org>
2024-11-11 21:00:06 +0100

    fix: move stuff to env file

--- frontend/scripts/content.js
+++ frontend/scripts/content.js
@@ -3,21 +3,31 @@ import path from "node:path";
 import fs from 'node:fs/promises';
 
 const CONTENT_PATH = './src/content/pages/';
-const NEXTCLOUD_PATH = '/Collectives/noIAA Website/content';
 
 function resetLine() {
   process.stdout.clearLine(0);
   process.stdout.cursorTo(0);
 }
 
+async function getEnvVariables() {
+  const env_raw = await fs.readFile('.env').then(b => b.toString());
+  const env = {};
+  for (const match of env_raw.matchAll(/^NEXTCLOUD_(?<key>.*)=(?<value>.*)$/gm)) {
+    const { key, value } = match.groups;
+    env[key] = value;
+  }
+  return env;
+}
+
 async function copy() {
-  const client = createClient('https://cloud.systemli.org/remote.php/dav/files/fili', {
-      username: 'fili',
-      password: 'JbmLn-RoiKt-FxCJP-8PqK3-KXDb5'
+  const env = await getEnvVariables();
+  const client = createClient(`${env.BASE_URL}/remote.php/dav/files/${env.USERNAME}`, {
+      username: env.USERNAME,
+      password: env.PASSWORD
   });
 
   process.stdout.write('Getting list of files...');
-  const files = await client.getDirectoryContents(NEXTCLOUD_PATH, { deep: true});
+  const files = await client.getDirectoryContents(env.PATH, { deep: true});
   resetLine();
   process.stdout.write(`Found ${files.filter(f => f.type === 'file').length} files in ${files.filter(f => f.type === 'directory').length} directories.\n`);
 
@@ -25,7 +35,7 @@ async function copy() {
     if (file.type !== 'file')
       continue
     const buff = await client.getFileContents(file.filename);
-    const filePath = file.filename.replace(`${NEXTCLOUD_PATH}/`, '')
+    const filePath = file.filename.replace(`${env.PATH}/`, '')
     const target = `${CONTENT_PATH}/${filePath}`;
     process.stdout.write(`Copying ${filePath}...`);
     await fs.mkdir(path.dirname(target), { recursive: true });
