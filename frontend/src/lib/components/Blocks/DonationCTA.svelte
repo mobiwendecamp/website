43b27281834d0747f13b1248c2533c70ab52b7d1
Sola <solalala@systemli.org>
2024-11-24 15:03:05 +0100

    Add Betterplace href

--- frontend/src/lib/components/Blocks/DonationCTA.svelte
+++ frontend/src/lib/components/Blocks/DonationCTA.svelte
@@ -42,7 +42,7 @@
                 <div class="py-4 space-y-2">
                     <h4 class="font-semibold">{t.donation_via_betterplace()}:</h4>
 
-                    <a class="font-semibold hover:underline flex items-center" target="_blank" href={''}>
+                    <a class="font-semibold hover:underline flex items-center" target="_blank" href={t.donation_via_betterplace_href()}>
                         <BetterplaceLogo class="h-8"/>
                     </a>
                 </div>
