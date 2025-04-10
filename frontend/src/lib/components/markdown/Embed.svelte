fa6f2b3c652e43cba1fd5f30af40e47b08caaddd
Tobias Hettler <tobias.hettler@bitbench.dev>
2025-04-10 19:28:49 +0200

    Add PraticapteForm

--- frontend/src/lib/components/markdown/Embed.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-    let {src} = $props()
-</script>
-
-<iframe class="border shadow w-full min-h-[35rem]" {src}/>
---
+++ frontend/src/lib/components/markdown/ParticipateForm.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    let {src} = $props()
+</script>
+
+<iframe class="border shadow w-full min-h-[35rem]" src="https://cloud.systemli.org/apps/forms/embed/LGPQD5LD3WMmfyWG4xrgFnGZ"/>
